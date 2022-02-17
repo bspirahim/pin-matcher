2b2a1ab276a77ec6bc112992cf6ea6eda4af4a82
Abdur-p-Rahim <bspirahim@gmail.com>
2022-02-16 21:33:14 -0800

    pin matcher done

--- js/app.js
+++ js/app.js
@@ -17,23 +17,6 @@ function generatePin(){
      document.getElementById('display-pin').value = pin;
 }
 
-/* document.getElementById('key-pad').addEventListener('click', function(event){
-     const number = event.target.innerText;
-     const clacInput = document.getElementById('typed-numbers');
-     if(isNaN(number)){
-          if(number == 'C'){
-               clacInput.value = '';
-          }
-     }
-     else{
- 
-          const previousNumber = clacInput.value;
-          const newNumber = previousNumber + number;
-          clacInput.value = newNumber;
-     }
-}) */
-
-
 const keypadButton = document.getElementById('key-pad');
 keypadButton.addEventListener('click', function(event){
      const number = event.target.innerText;
@@ -51,7 +34,20 @@ keypadButton.addEventListener('click', function(event){
           clacInput.value = newNumber;
      }
 
-
-     
-    
 })
+
+function verifyPin(){
+     const pin = document.getElementById('display-pin').value;
+     const typedNumbers = document.getElementById('typed-numbers').value;
+     const successMessage = document.getElementById('notify-success');
+     const failedMessage = document.getElementById('notify-failed');
+     if(pin == typedNumbers){
+          successMessage.style.display = 'block';
+          failedMessage.style.display = 'none';
+     }
+     else{
+         
+          failedMessage.style.display = 'block';
+          successMessage.style.display = 'none';
+     }
+}
